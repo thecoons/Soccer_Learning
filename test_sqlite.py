0c9a778776c7b5e960ffc7465d7dd9b343bd2963
Your Name <you@example.com>
2017-09-10 12:48:41 +0200

    Expérimentation sur les champs de match

--- test_sqlite.py
+++ test_sqlite.py
@@ -1,16 +1,26 @@
-import pandas as pd
+"""Database Script."""
 import sqlite3
 
+import pandas as pd
+
+# pylint: disable=invalid-name
+
+
 def load_table(table_name, connecter):
-    return pd.read_sql_query("SELECT * FROM {}".format(table_name), con)
+    """Load table from database to pandas Dataframe"""
+    return pd.read_sql_query("SELECT * FROM {}".format(table_name), connecter)
 
-con = sqlite3.connect('database/database.sqlite')
 
-# Load data in to DataFrame
-player_df = load_table('Player', con)
-match_df = load_table('Match', con)
-# Close DB pipe
-con.close
+if __name__ == "__main__":
+    con = sqlite3.connect('database/database.sqlite')
 
-print(player_df.head())
-print(match_df.head())
+    # Load data in to DataFrame
+    match_df = load_table('Match', con)
+
+    # Close DB pipe
+    con.close()
+
+    match_2016 = match_df[(match_df['season'] == '2015/2016') & (match_df['league_id'] == 1729)]
+    sliced_match_df = match_2016[['goal']]
+
+    sliced_match_df.to_csv(path_or_buf='output.csv')
